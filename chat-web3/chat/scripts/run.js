a7435d989f2754f05a303337d153ba74b82e9469
dev-araujo <theriancirculo@protonmail.com>
2022-08-15 10:06:29 -0300

    feat: web3-chat

---
+++ chat-web3/chat/scripts/run.js
@@ -0,0 +1,32 @@
+const main = async () => {
+  const waveContractFactory = await hre.ethers.getContractFactory("WavePortal");
+  const waveContract = await waveContractFactory.deploy();
+  await waveContract.deployed();
+  console.log("Endereço do contrato:", waveContract.address);
+
+  let waveCount;
+  waveCount = await waveContract.getTotalWaves();
+  console.log(waveCount.toNumber());
+
+  let waveTxn = await waveContract.wave("Uma mensagem!");
+  await waveTxn.wait();
+
+  const [_, randomPerson] = await hre.ethers.getSigners();
+  waveTxn = await waveContract.connect(randomPerson).wave("Outra mensagem!");
+  await waveTxn.wait();
+
+  let allWaves = await waveContract.getAllWaves();
+  console.log(allWaves);
+};
+
+const runMain = async () => {
+  try {
+    await main();
+    process.exit(0);
+  } catch (error) {
+    console.log(error);
+    process.exit(1);
+  }
+};
+
+runMain();
