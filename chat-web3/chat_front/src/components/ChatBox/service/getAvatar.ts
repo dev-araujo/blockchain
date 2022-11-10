6eb43636ac36e4e1a44c935cc535db4577747cfe
dev-araujo <theriancirculo@protonmail.com>
2022-11-10 06:51:51 -0300

    feat: change character

--- chat-web3/chat_front/src/components/ChatBox/service/getAvatar.ts
+++ chat-web3/chat_front/src/components/ChatBox/service/getAvatar.ts
@@ -1,17 +1,16 @@
 import api from "./api"
 
- 
- const getAvatar = (address:string,avatarUrl:any) => {
-    api
-      .get(`bottts/${address}.svg`)
-      .then((response) => {
-        const blob = new Blob([response.data], { type: "image/svg+xml" })
-        const url = URL.createObjectURL(blob)
-        avatarUrl(url)
-      })
-      .catch((err) => {
-        console.error("ops! ocorreu um erro" + err)
-      })
-  }
+const getAvatar = (address: string, avatarUrl: any) => {
+  api
+    .get(`big-smile/${address}.svg`)
+    .then((response) => {
+      const blob = new Blob([response.data], { type: "image/svg+xml" })
+      const url = URL.createObjectURL(blob)
+      avatarUrl(url)
+    })
+    .catch((err) => {
+      console.error("ops! ocorreu um erro" + err)
+    })
+}
 
-  export default getAvatar
+export default getAvatar
